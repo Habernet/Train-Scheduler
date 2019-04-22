938a8b2b3efcb163931da2b3502df01e98cc4494
Patrick Haberern <patrick@Patricks-MBP.nc.rr.com>
2019-04-22 16:24:06 -0400

    Initial commit, libraries linked.

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAEeCpHM-BdMVxbbEuL7ER6GeHLVrbbL3g",
+    authDomain: "train-scheduler-8a4e8.firebaseapp.com",
+    databaseURL: "https://train-scheduler-8a4e8.firebaseio.com",
+    projectId: "train-scheduler-8a4e8",
+    storageBucket: "train-scheduler-8a4e8.appspot.com",
+    messagingSenderId: "65351513165"
+  };
+  firebase.initializeApp(config);
